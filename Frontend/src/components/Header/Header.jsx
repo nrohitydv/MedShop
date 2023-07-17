4ac992d47d6da9e3c8ed6fd1ae66ef509e4a168b
Rohit <rohityadav787898@gmail.com>
2023-07-17 22:08:20 +0545

    simple header created and still working

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useRef } from "react";
+import userImg from "../../assets/images/avatar-icon.png";
 import logo from "..//../assets/images/logo.png";
+import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
 
+const navLinks = [
+  { path: "/home", display: "Home" },
+  { path: "/doctors", display: "Find a Doctor" },
+  { path: "/services", display: "Services" },
+  { path: "/contact", display: "Contact" },
+];
 function Header() {
   return (
     <header className="header flex items-center">
@@ -7,6 +17,41 @@ function Header() {
         <div className="flex items-center justify-between">
           <div>
             <img src={logo} alt="" />
+          </div>
+          <div className="navigation">
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                        : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex items-center gap-4">
+            <div>
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                  <img src={userImg} className="w-full rounded-full" alt="" />
+                </figure>
+              </Link>
+            </div>
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                Login
+              </button>
+            </Link>
+            <span>
+              <BiMenu className="w-6 h-6 cursor-pointer"></BiMenu>
+            </span>
           </div>
         </div>
       </div>
