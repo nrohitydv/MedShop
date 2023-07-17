5e1f44eaab0e9dbdaebde9f451aca07a7eee4217
Rohit <rohityadav787898@gmail.com>
2023-07-17 20:59:26 +0545

    some styling done and working on header

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,5 +1,17 @@
+import logo from "..//../assets/images/logo.png";
+
 function Header() {
-  return <div>header</div>;
+  return (
+    <header className="header flex items-center">
+      <div className="conatiner">
+        <div className="flex items-center justify-between">
+          <div>
+            <img src={logo} alt="" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
