1904deefc04f18b29d1db062da1ee5635f35c437
Rohit <rohityadav787898@gmail.com>
2023-07-20 13:24:15 +0545

    working on home page

---
+++ Frontend/src/components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import aboutImg from "../../assets/images/about.png";
+import aboutCardImg from "../../assets/images/about-card.png";
+
+function About() {
+  return (
+    <section>
+      <div className="container">
+        <div className="flex items-center justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row ">
+          <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1 ">
+            <img src={aboutImg} alt="" />
+            <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] ">
+              ,<img src={aboutCardImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,58 +1,137 @@
 import heroImg01 from "../assets/images/hero-Img01.png";
 import heroImg02 from "../assets/images/hero-Img02.png";
 import heroImg03 from "../assets/images/hero-Img03.png";
+import icon01 from "../assets/images/icon01.png";
+import { Link } from "react-router-dom";
+import icon02 from "../assets/images/icon02.png";
+import icon03 from "../assets/images/icon03.png";
+import { BsArrowRight } from "react-icons/bs";
+import About from "../components/About/About";
 
 function Home() {
   return (
     <>
-      <>
-        <section className="hero__section pt-[60px] 2xl:h-[800px]">
-          <div className="container">
-            <div className="flex flex-col lg:flex-row gap-[90px] items:center justify-between">
-              <div>
-                <div className="lg:-[570px]">
-                  <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px] ">
-                    We help patients live a healty, long life.
-                  </h1>
-                  <button className="btn">Request an Appoinment</button>
-                </div>
-                <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
-                  <div>
-                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
-                      30+
-                    </h2>
-                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
-                    <p className="text__para">Years of Experiance</p>
-                  </div>
-                  <div>
-                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
-                      15+
-                    </h2>
-                    <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
-                    <p className="text__para">Clinic Location</p>
-                  </div>
-                  <div>
-                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
-                      100%
-                    </h2>
-                    <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
-                    <p className="text__para">Patient Satisfication</p>
-                  </div>
-                </div>
+      <section className="hero__section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items:center justify-between">
+            <div>
+              <div className="lg:-[570px]">
+                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px] ">
+                  We help patients live a healty, long life.
+                </h1>
+                <button className="btn">Request an Appoinment</button>
               </div>
-              <div className="flex gap-[30px] justify-end">
+              <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
                 <div>
-                  <img className="w-full" src={heroImg01} alt="" />
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    30+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Years of Experiance</p>
                 </div>
-                <div className="mt-[30px]">
-                  <img className="w-full mb-[30px] " src={heroImg02} alt="" />
-                  <img className="w-full" src={heroImg03} alt="" />
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    15+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Clinic Location</p>
+                </div>
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    100%
+                  </h2>
+                  <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Patient Satisfication</p>
                 </div>
               </div>
             </div>
+            <div className="flex gap-[30px] justify-end">
+              <div>
+                <img className="w-full" src={heroImg01} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <img className="w-full mb-[30px] " src={heroImg02} alt="" />
+                <img className="w-full" src={heroImg03} alt="" />
+              </div>
+            </div>
           </div>
-        </section>
-      </>
+        </div>
+      </section>
+
+      <section className="container">
+        <div className="lg:w-[470px] mx-auto ">
+          <h2 className="heading text-center">
+            Providing the Best medical services.
+          </h2>
+          <p className="text__para text-center">
+            World class care for everyone. Our health System offers unmatched,
+            expert health care.{" "}
+          </p>
+        </div>
+        <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap[30px] mt-[30px] lg:mt-[55px]">
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon01} alt="" />
+            </div>
+            <div className="mt-[30px] ">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center ">
+                Find a Doctor
+              </h2>
+              <p className="text-[16px leading-7 text-textColor font-[400] mt-4 text-center">
+                World-class care for everyone.Our health System offers
+                unmatched,expert health care.From the lab to clinic.
+              </p>
+              <Link
+                to="/doctors"
+                className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30x] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              >
+                <BsArrowRight className="group-hover:text-white w-6 h-5" />
+              </Link>
+            </div>
+          </div>
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon02} alt="" />
+            </div>
+            <div className="mt-[30px] ">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center ">
+                Find a Location
+              </h2>
+              <p className="text-[16px leading-7 text-textColor font-[400] mt-4 text-center">
+                World-class care for everyone.Our health System offers
+                unmatched,expert health care.From the lab to clinic.
+              </p>
+              <Link
+                to="/doctors"
+                className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30x] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              >
+                <BsArrowRight className="group-hover:text-white w-6 h-5" />
+              </Link>
+            </div>
+          </div>
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon03} alt="" />
+            </div>
+            <div className="mt-[30px] ">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center ">
+                Book Appoinment
+              </h2>
+              <p className="text-[16px leading-7 text-textColor font-[400] mt-4 text-center">
+                World-class care for everyone.Our health System offers
+                unmatched,expert health care.From the lab to clinic.
+              </p>
+              <Link
+                to="/doctors"
+                className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30x] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              >
+                <BsArrowRight className="group-hover:text-white w-6 h-5" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <About />
     </>
   );
 }
