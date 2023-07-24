24c7c3407d7320ea48aa12187da950fc316ace54
Rohit <rohityadav787898@gmail.com>
2023-07-24 17:17:25 +0545

    still working on Home

--- Frontend/src/components/Services/ServiceCard.jsx
+++ Frontend/src/components/Services/ServiceCard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({ item, index }) => {
   const { name, desc, bgColor, textColor } = item;
@@ -11,6 +13,24 @@ const ServiceCard = ({ item, index }) => {
       <p className="text-[16px] leading-7 font-[400] text-textColor mt-4 ">
         {desc}
       </p>
+      <div className="flex items-center justify-between mt-[30px] ">
+        <Link
+          to="/doctors"
+          className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E]  flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+        >
+          <BsArrowRight className="group-hover:text-white w-6 h-5" />
+        </Link>
+        <span
+          className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]"
+          style={{
+            background: `${bgColor}`,
+            color: `${textColor}`,
+            borderRadius: "60px 0 0 6px",
+          }}
+        >
+          {index + 1}
+        </span>
+      </div>
     </div>
   );
 };
