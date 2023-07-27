2f787bbd90ef5ca538cc842accfb2ff1d803450f
Rohit <rohityadav787898@gmail.com>
2023-07-27 16:45:11 +0545

    footer added

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,37 @@
-function Footer() {
-  return <div>footer</div>;
-}
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-600">
+          Doctor Appointment Booking Website &copy; 2023
+        </p>
+        <div className="flex justify-center mt-2">
+          <a
+            href="#"
+            className="text-gray-600 hover:text-blue-500 transition duration-300 mx-2"
+          >
+            Terms of Service
+          </a>
+          <span className="text-gray-600">|</span>
+          <a
+            href="#"
+            className="text-gray-600 hover:text-blue-500 transition duration-300 mx-2"
+          >
+            Privacy Policy
+          </a>
+          <span className="text-gray-600">|</span>
+          <a
+            href="/contact"
+            className="text-gray-600 hover:text-blue-500 transition duration-300 mx-2"
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
